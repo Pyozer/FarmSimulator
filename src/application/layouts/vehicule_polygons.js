5013eeda3ae0a5324fa193e65838ad4a850d6df0
Pyozer <jeancharles.msse@gmail.com>
2017-05-22 21:04:34 +0200

    Ajout des marqueurs véhicules

--- src/application/layouts/vehicule_polygons.js
+++ src/application/layouts/vehicule_polygons.js
@@ -4,31 +4,49 @@ var data = {
         {
             "type":"Feature",
             "properties":{
-                "VEHICULE_ID":1,
+                "VEHICULE_ID": 1,
                 "type":"Moissonneuse",
-                "etat":"Utilise",
+                "icon": "../images/marker_moissonneuse.png",
+                "etat":"Utilisé",
             },
             "geometry":{
                 "type":"Point",
-                "coordinates":[
-                    -1.448450,
-                    47.970787
-                ],
+                "coordinates": [
+                  -1.4737987518310547,
+                  47.95302998186228
+                ]
             }
         },
         {
             "type":"Feature",
             "properties":{
-                "VEHICULE_ID":2,
+                "VEHICULE_ID": 2,
                 "type":"Tracteur",
+                "icon": "../images/marker_tracteur.png",
                 "etat":"Utilise",
             },
             "geometry":{
                 "type":"Point",
-                "coordinates":[
-                    -1.448450,
-                    47.970787
-                ],
+                "coordinates": [
+                  -1.4719104766845703,
+                  47.95096050883636
+                ]
+            }
+        },
+        {
+            "type":"Feature",
+            "properties":{
+                "VEHICULE_ID":3,
+                "type":"Botteleuse",
+                "icon": "../images/marker_botteleuse.png",
+                "etat":"Utilise",
+            },
+            "geometry":{
+                "type":"Point",
+                "coordinates": [
+                  -1.4736270904541016,
+                  47.94894844171735
+                ]
             }
         },
     ]
