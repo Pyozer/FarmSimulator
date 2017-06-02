dc95d6ad3635ad467999dd7ca84d7002ee95ca4b
justin <justin.martin.etu@univ-lemans.fr>
2017-06-02 17:29:02 +0200

    update commandeController

--- src/application/controlleurs/AddCommandeController.java
+++ src/application/controlleurs/AddCommandeController.java
@@ -1,11 +1,20 @@
 package application.controlleurs;
 
 import application.classes.AlertDialog;
+import application.modeles.Champ;
+import application.modeles.ChampSQL;
 import application.modeles.ClientSQL;
+import application.modeles.Culture;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.layout.BorderPane;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 /**
  * Controlleur de la vue de la gestion des clients de l'ETA
@@ -15,11 +24,11 @@ public class AddCommandeController {
     /** Layout **/
     @FXML private BorderPane bpane;
     /** Elements **/
-    @FXML private JFXTextField nom_client;
-    @FXML private JFXTextField prenom_client;
-    @FXML private JFXTextField tel_client;
-    @FXML private JFXTextField adresse_client;
-    @FXML private JFXTextField email_client;
+    @FXML private JFXDatePicker date_commande;
+    @FXML private JFXComboBox<Champ> liste_champs;
+    @FXML private JFXComboBox liste_transport;
+    @FXML private JFXComboBox liste_type_bott;
+
 
     /**
      * Initializes the controller class.
@@ -27,26 +36,37 @@ public class AddCommandeController {
     public void initialize() {
         bpane.setOnMouseClicked(e -> bpane.requestFocus());
 
+        ChampSQL champSQL = new ChampSQL();
+        List<Champ> listChamp = champSQL.getChampsList();
+        for(Champ champ : listChamp)
+            liste_champs.getItems().add(champ);
+
+        liste_transport.getItems().addAll("Le client", "ETA", "Le négociant");
+
+        liste_type_bott.getItems().addAll("Non demandé", "Ronde", "Carré");
     }
 
     @FXML
-    public void handleSubmitClient() {
-        String inputNom = nom_client.getText();
-        String inputPrenom = prenom_client.getText();
-        String inputTel = tel_client.getText();
-        String inputAdresse = adresse_client.getText();
-        String inputEmail = email_client.getText();
+    public void handleSubmitCommande() {
+        LocalDate inputDateBadFormat = date_commande.getValue();
+        DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String inputDate = inputDateBadFormat.format(formatDate);
 
-        if(inputNom.isEmpty() || inputPrenom.isEmpty() || inputTel.isEmpty() || inputAdresse.isEmpty() || inputEmail.isEmpty()) {
+        Champ inputChamp = liste_champs.getValue();
+
+        String inputTransport = "" + liste_transport.getValue();
+        String inputTypeBott = "" + liste_type_bott.getValue();
+
+        if(inputDate.isEmpty() || inputTransport.isEmpty() || inputTypeBott.isEmpty()) { //champs
             AlertDialog alert = new AlertDialog("Erreur", null, "Vous devez remplir tous les champs de texte !", Alert.AlertType.ERROR);
             alert.show();
-        } else {
+        } /*else {
             ClientSQL clientSQL = new ClientSQL();
             clientSQL.addClient(inputNom, inputPrenom, inputTel, inputAdresse, inputEmail);
 
             AlertDialog alert = new AlertDialog("Succès", null, "Le client à bien été ajouté !", Alert.AlertType.CONFIRMATION);
             alert.show();
-        }
+        }*/
     }
 
 
