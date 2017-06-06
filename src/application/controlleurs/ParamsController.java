9379b64d2254055892294fab09e666d6d23df648
justin <justin.martin.etu@univ-lemans.fr>
2017-06-06 08:29:07 +0200

    creation paramètre

--- src/application/controlleurs/ParamsController.java
+++ src/application/controlleurs/ParamsController.java
@@ -27,25 +27,16 @@ public class ParamsController {
     public void initialize() {
         bpane.setOnMouseClicked(e -> bpane.requestFocus());
 
-        /*try {
+        try {
             BorderPane tmpPane = FXMLLoader.load(getClass().getResource(Constant.LAYOUT_PATH  + "params_bdd.fxml"));
             bdd_tab.setContent(tmpPane);
 
-            tmpPane = FXMLLoader.load(getClass().getResource("/layouts/params_infos.fxml"));
+            tmpPane = FXMLLoader.load(getClass().getResource(Constant.LAYOUT_PATH  + "params_infos.fxml"));
             info_tab.setContent(tmpPane);
 
          } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
     }
 
-    @FXML
-    public void bddTabClicked() throws IOException {
-        bdd_tab.setContent(FXMLLoader.load(getClass().getResource(Constant.LAYOUT_PATH  + "params_bdd.fxml")));
-    }
-
-    @FXML
-    public void etaTabClicked() throws IOException {
-        info_tab.setContent(FXMLLoader.load(getClass().getResource(Constant.LAYOUT_PATH  + "params_infos.fxml")));
-    }
 }
