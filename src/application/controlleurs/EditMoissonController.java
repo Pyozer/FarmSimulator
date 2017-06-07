e2ca7ab64a020e4fb802b7f4e21c4f59a0a5b93d
justin <justin.martin.etu@univ-lemans.fr>
2017-06-07 17:37:40 +0200

    moisson

--- src/application/controlleurs/EditMoissonController.java
+++ src/application/controlleurs/EditMoissonController.java
@@ -2,6 +2,9 @@ package application.controlleurs;
 
 import application.classes.AlertDialog;
 import application.modeles.ClientSQL;
+import application.modeles.Commande;
+import application.modeles.MoissonSQL;
+import application.modeles.Vehicule;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextField;
@@ -10,7 +13,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.layout.BorderPane;
 
-public class AddMoissonController {
+public class EditMoissonController {
 
     /** Layout **/
     @FXML private BorderPane bpane;
@@ -22,6 +25,9 @@ public class AddMoissonController {
     @FXML private JFXDatePicker date_fin;
     @FXML private JFXTimePicker time_debut;
     @FXML private JFXTimePicker time_fin;
+
+    private Commande selectedCommande;
+    private Vehicule selectedVehicule;
     /**
      * Initializes the controller class.
      */
@@ -29,25 +35,31 @@ public class AddMoissonController {
         bpane.setOnMouseClicked(e -> bpane.requestFocus());
     }
 
+
     @FXML
     public void handleSubmitCommande() {
 
-        /*String inputPoidRecolte= poid_recolte.getText();
-        String inputNbKilo = nb_Kilo.getText();
+        Float inputPoidRecolte= Float.parseFloat(poid_recolte.getText());
+        Float inputNbKilo = Float.parseFloat(nb_Kilo.getText());
         String inputDateDebut = date_debut.getValue().toString();
         String inputDateFin = date_fin.getValue().toString();
         String inputTimeDebut = time_debut.getValue().toString();
         String inputTimeFin = time_fin.getValue().toString();
 
-        if (inputNom.isEmpty() || inputPrenom.isEmpty() || inputTel.isEmpty() || inputAdresse.isEmpty() || inputEmail.isEmpty()) {
+        if (inputNbKilo.isEmpty() || inputPoidRecolte.isEmpty() || inputDateDebut.isEmpty() || inputDateFin.isEmpty() || inputTimeDebut.isEmpty() || inputTimeFin.isEmpty()) {
             AlertDialog alert = new AlertDialog("Erreur", null, "Vous devez remplir tous les champs de texte !", Alert.AlertType.ERROR);
             alert.show();
         } else {
-            ClientSQL.addClient(inputNom, inputPrenom, inputTel, inputAdresse, inputEmail);
+            MoissonSQL.editMoisson(selectedCommande, selectedVehicule, inputDateDebut, inputTimeDebut, inputDateFin, inputTimeFin, inputNbKilo, inputPoidRecolte);
 
             AlertDialog alert = new AlertDialog("Succès", null, "Le client à bien été ajouté !", Alert.AlertType.CONFIRMATION);
             alert.show();
-        }*/
+        }
+    }
+
+    public void defineVariableMoisson(Commande commande, Vehicule vehicule) {
+        this.selectedCommande = commande;
+        this.selectedVehicule = vehicule;
     }
 
 }
--- src/application/modeles/MoissonSQL.java
+++ src/application/modeles/MoissonSQL.java
@@ -1,15 +1,20 @@
 package application.modeles;
 
+import application.classes.JSONManager;
+import application.classes.Point;
+import application.classes.Polygon;
 import application.database.DBConnection;
 import application.database.NamedParameterStatement;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-/**
- * Classe pour la gestion des rapports de moissons
- */
 public class MoissonSQL {
 
-    public static void addMoisson(Commande inputCommande, Vehicule inputVehicule, String intputJ_debut, String inputH_debut, String inputJ_fin, String inputH_fin, Float inputNbKilo, Float inputNbTonne) {
+    public static void editMoisson(Commande inputCommande, Vehicule inputVehicule, String intputJ_debut, String inputH_debut, String inputJ_fin, String inputH_fin, Float inputNbKilo, Float inputNbTonne) {
         String request = "UPDATE Ordre SET tonnes=:tonnes, nb_km_ordre=:nbKilo, duree_ordre:duree, heure_arrive_ordre:heureArrive WHERE id_vehi=:vehi AND id_com=:com";
 
         try {
@@ -32,18 +37,14 @@ public class MoissonSQL {
 
     }
 
-    public static void editCommande(Commande commande) {
-            String request = "UPDATE Commande SET date_com=:date_com, bott_com=:bott_com, transp_com=:transp_com, taille_max_transp_com=:taille_max_transp_com, id_champ=:id_champ WHERE id_com=:id_com";
+    public static void addMoisson(Commande inputCommande, Vehicule inputVehicule) {
+            String request = "INSERT INTO Ordre(id_vehi, id_com) VALUES (:id_vehi, :id_com)";
 
         try {
             NamedParameterStatement preparedStatement = new NamedParameterStatement(DBConnection.getConnection(), request);
 
-            preparedStatement.setString("date_com", commande.getDate().toString());
-            preparedStatement.setString("bott_com", commande.getTypebott());
-            preparedStatement.setString("transp_com", commande.getTransport());
-            preparedStatement.setFloat("taille_max_transp_com", commande.getTaillemax());
-            preparedStatement.setInt("id_champ", commande.getChampCommande().getId());
-            preparedStatement.setInt("id_com", commande.getId());
+            preparedStatement.setString("id_vehi", "" + inputVehicule.getId());
+            preparedStatement.setString("id_com", "" + inputCommande.getId());
 
             // Execute SQL statement
             preparedStatement.executeUpdate();
@@ -54,4 +55,130 @@ public class MoissonSQL {
             System.err.println(ex.getMessage());
         }
     }
+
+    public static ObservableList<Moisson> getMoissonList() {
+
+        String request = "SELECT * FROM Ordre " +
+                        "INNER JOIN Commande ON Commande.id_com = Ordre.id_com"+
+                        "INNER JOIN Champ ON Champ.id_champ=Commande.id_champ " +
+                        "INNER JOIN Agriculteur ON Agriculteur.id_agri=Champ.id_agri" +
+                        "INNER JOIN Culture ON Culture.id_cul=Champ.type_champ ";
+
+        ObservableList<Moisson> moissonList = FXCollections.observableArrayList();
+
+        try {
+            PreparedStatement preparedStatement = DBConnection.getConnection().prepareStatement(request);
+            // Execute SQL statement
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+
+                Vehicule vehi_com = null;
+
+                Point coord_center = JSONManager.readPoint(rs.getString("coord_centre_champ"));
+                Polygon coord_champ = new Polygon(JSONManager.readPolygon(rs.getString("coords_champ")));
+                Point position = JSONManager.readPoint(rs.getString("position_vehi"));
+
+                if(rs.getString("type_bott") != null){
+                    vehi_com = new Botteleuse(
+                                            Integer.parseInt(rs.getString("id_vehi")),
+                                            rs.getString("marque_vehi"),
+                                            rs.getString("modele_vehi"),
+                                            rs.getString("etat_vehi"),
+                                            position,
+                                            Boolean.parseBoolean(rs.getString("type_bott"))
+                    );
+                }
+                else if(rs.getString("taille_tremis_moi") != null){
+                    vehi_com = new Moissonneuse(
+                            Integer.parseInt(rs.getString("id_vehi")),
+                            rs.getString("marque_vehi"),
+                            rs.getString("modele_vehi"),
+                            rs.getString("etat_vehi"),
+                            position,
+                            Integer.parseInt(rs.getString("taille_tremis_moi")),
+                            Integer.parseInt(rs.getString("taille_reserve_moi")),
+                            Integer.parseInt(rs.getString("largeur_route_moi")),
+                            Integer.parseInt(rs.getString("hauteur_moi")),
+                            Integer.parseInt(rs.getString("largeur_coupe_moi")),
+                            Integer.parseInt(rs.getString("conso_fonct_moi")),
+                            Integer.parseInt(rs.getString("conso_route_moi")),
+                            Integer.parseInt(rs.getString("poids_moi"))
+                    );
+                }
+                else if(rs.getString("cap_rem_tract") != null){
+                    vehi_com = new Tracteur(
+                            Integer.parseInt(rs.getString("id_vehi")),
+                            rs.getString("marque_vehi"),
+                            rs.getString("modele_vehi"),
+                            rs.getString("etat_vehi"),
+                            position,
+                            Integer.parseInt(rs.getString("cap_rem_tract"))
+                    );
+                }
+
+                    moissonList.add(new Moisson(Integer.parseInt(rs.getString("id_ordre")),
+                                        new Commande(
+                                                Integer.parseInt(rs.getString("id_com")),
+                                                rs.getString("transp_com"),
+                                                rs.getString("bott_com"),
+                                                Float.parseFloat(rs.getString("taille_max_transp_com")),
+                                                rs.getString("date_com"),
+                                                Float.parseFloat(rs.getString("tonne_com")),
+                                                Float.parseFloat(rs.getString("cout_com")),
+                                                new Champ(
+                                                        Integer.parseInt(rs.getString("id_agri")),
+                                                        Float.parseFloat(rs.getString("surf_champ")),
+                                                        rs.getString("adr_champ"),
+                                                        coord_center,
+                                                        coord_champ,
+                                                        rs.getString("type_cul"),
+                                                        new Agriculteur(
+                                                                Integer.parseInt(rs.getString("id_agri")),
+                                                                rs.getString("prenom_agri"),
+                                                                rs.getString("nom_agri"),
+                                                                rs.getString("tel_agri"),
+                                                                rs.getString("adr_agri"),
+                                                                rs.getString("email_agri")
+                                                        )
+                                                )
+                                        ),
+                                        vehi_com,
+                                        rs.getString("heure_arrive_ordre"),
+                                        rs.getString("duree_ordre"),
+                                        Float.parseFloat(rs.getString("nb_km_ordre")),
+                                        Float.parseFloat(rs.getString("tonnes_ordre"))
+                                    ));
+
+
+
+            }
+
+            rs.close();
+            preparedStatement.close();
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+        return moissonList;
+    }
+
+   /* public static ObservableList<Moisson> getMoissonList() {
+        return getMoissonList();
+    }*/
+
+    public static void deleteMoisson(Moisson moisson) {
+        String request = "DELETE FROM Ordre WHERE id_com=:id";
+
+        try {
+            NamedParameterStatement preparedStatement = new NamedParameterStatement(DBConnection.getConnection(), request);
+            preparedStatement.setInt("id", moisson.getId());
+            preparedStatement.executeUpdate();
+
+            preparedStatement.close();
+
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
 }
