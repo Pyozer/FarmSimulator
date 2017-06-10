0296597f010db3aa41ac661bb508b207c83323c9
Pyozer <jeancharles.msse@gmail.com>
2017-06-11 01:44:00 +0200

    Edit

--- src/application/controlleurs/parametre/ParamsInfosController.java
+++ src/application/controlleurs/parametre/ParamsInfosController.java
@@ -105,7 +105,7 @@ public class ParamsInfosController {
     }
 
 	private void loadLogin() {
-        SwitchView switchView = new SwitchView("parametre/home_login", Constant.HOME_LOGIN);
+        SwitchView switchView = new SwitchView("parametre/home_login", Constant.HOME_LOGIN_TITLE);
         switchView.showScene();
 	}
 
